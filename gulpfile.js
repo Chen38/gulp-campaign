e50721510e23e115faa50b4c2fde1a3004981f14
Barry Chen <spchen38@163.com>
2016-12-12 17:18:14 +0800

    use gulp-load-plugins to organize all gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,16 @@
 const gulp = require('gulp');
-const plumber = require('gulp-plumber'); // prevent error interrupt task
-const concat = require('gulp-concat'); // merge scripts
-const uglify = require('gulp-uglify'); // minify scripts
+const plugins = require('gulp-load-plugins')(); // load all gulp- plugins
 const pump = require('pump'); // pipe streams
-const rename = require('gulp-rename'); // rename file
 const amdOptimize = require("amd-optimize"); // AMD pack
-const replace = require('gulp-replace'); // build replace path
-const spritesmith = require('gulp.spritesmith'); // sprite convert
-const htmlmin = require('gulp-htmlmin'); // minify html
-const compass = require('gulp-compass'); // compile sass to css
 const browserSync = require('browser-sync').create(); // auto refresh browser
 const reload = browserSync.reload;
+// define config
 const config = require('./config');
 
 // move bower components
 gulp.task('moveBowerComponents', () => {
 	gulp.src(config.dev.bower.requirejs)
-		.pipe(uglify())
+		.pipe(plugins.uglify())
 		.pipe(gulp.dest(config.dev.dest.js));
 
 	gulp.src(config.dev.bower.zepto)
@@ -29,35 +23,16 @@ gulp.task('moveBowerComponents', () => {
 		.pipe(gulp.dest(config.dev.dest.css));
 });
 
-// concat and uglify js
-// gulp.task('uglifyLib', () => {
-// 	pump([
-// 		gulp.src('vendor/js/*.js'),
-// 		concat('fx.min.js'),
-// 		uglify(),
-// 		gulp.dest('dist/js')
-// 	]);
-// });
-
-// gulp.task('uglifyApp', () => {
-// 	pump([
-// 		gulp.src('scripts/*'),
-// 		concat('app.min.js'),
-// 		uglify(),
-// 		gulp.dest('dist/js')
-// 	]);
-// });
-
 // compile sass
 gulp.task('sass', () => {
 	// return watch('sass/*.scss', () => {
 	gulp.src('sass/*.scss')
-		.pipe(plumber({
+		.pipe(plugins.plumber({
 			errorHandler: (err) => {
 				// console.log(err.message);
 			}
 		}))
-		.pipe(compass(config.compass))
+		.pipe(plugins.compass(config.compass))
 		.pipe(gulp.dest('css'))
 		.pipe(browserSync.stream());
 	// })
@@ -69,7 +44,10 @@ gulp.task('sass', () => {
 // auto refresh browser
 gulp.task('refresh', ['sass'], () => {
 	browserSync.init({
-		// you can change your own Apache server ip address
+		/**
+		 * you should change your own ip address,
+		 * like ip + port
+		 */
 		'proxy': "http://10.146.67.187:8038/",
 		'notify': false
 	});
@@ -81,15 +59,15 @@ gulp.task('refresh', ['sass'], () => {
 // requirejs concat
 gulp.task('amd', () => {
 	gulp.src('js/*.js')
-		.pipe(plumber({
+		.pipe(plugins.plumber({
 			errorHandler: err => {
 				console.log(err.message);
 			}
 		}))
 		.pipe(amdOptimize('main', config.requirejs))
-		.pipe(concat('app.js'))
-		.pipe(uglify())
-		.pipe(rename({
+		.pipe(plugins.concat('app.js'))
+		.pipe(plugins.uglify())
+		.pipe(plugins.rename({
 			'suffix': '.min'
 		}))
 		.pipe(gulp.dest('dist/js'));
@@ -101,7 +79,7 @@ gulp.task('dev', ['moveBowerComponents', 'refresh']);
 // sprite
 gulp.task('generateSprite', () => {
 	let spriteData = gulp.src('sprite/src/*')
-		.pipe(spritesmith(config.spritesmith));
+		.pipe(plugins.spritesmith(config.spritesmith));
 	return spriteData
 		.pipe(gulp.dest('sprite'));
 });
@@ -116,29 +94,17 @@ gulp.task('sprite', ['generateSprite'], () => {
 	]);
 });
 
-// tiny
-// gulp.task('tiny', () => {
-// 	imagemin(['img/*', 'img/**/*'], 'dist/img', {
-// 		plugins: [
-// 			imageminMozjpeg({
-// 				progressive: true
-// 			}),
-// 			imageminPngquant()
-// 		]
-// 	});
-// });
-
 // build
 gulp.task('build', ['amd'], () => {
 	pump([
 		gulp.src('test.html'),
-		htmlmin(config.htmlmin),
-		rename({
+		plugins.htmlmin(config.htmlmin),
+		plugins.rename({
 			basename: 'index'
 		}),
 		// replace(/(\.\.\/)/g, ''),
 		gulp.dest('./'),
-		replace(/(js\/main)/, config.build.js.dist),
+		plugins.replace(/(js\/main)/, config.build.js.dist),
 		gulp.dest('./')
 	]);
 });
