20dbf3f729a35599e5bd3f2b5ab9a53b124edf36
Barry Chen <spchen38@163.com>
2017-05-04 17:23:19 +0800

    use browserify to bundle js and other little changes

--- config.js
+++ config.js
@@ -1,38 +1,8 @@
 module.exports = {
-	'dev': {
-		'bower': {
-			'requirejs': 'bower_components/requirejs/require.js',
-			// 'zepto': 'bower_components/zepto/zepto.min.js',
-			'jquery': 'bower_components/jquery/dist/jquery.min.js',
-			'fastclick': 'bower_components/fastclick/lib/fastclick.js',
-			'swiper': 'bower_components/swiper/dist/js/swiper.jquery.min.js'
-			// 'normalize': 'bower_components/normalize-css/normalize.css'
-		},
-		'dest': {
-			'js': 'vendor/js',
-			'css': 'vendor/css'
-		}
-	},
-	'build': {
-		'js': {
-			'dist': 'dist/js/app.min'
-		}
-	},
 	'compass': {
 		'config_file': 'config.rb',
-		'css': 'css',
-		'sass': 'sass'
-	},
-	'requirejs': {
-		'paths': {
-			'$': 'vendor/js/zepto.min',
-			'wx': 'vendor/js/jweixin-1.0.0',
-			'fastclick': 'vendor/js/fastclick'
-		},
-		'shim': {
-			'js/app': ['$'],
-			'js/fn': ['$']
-		}
+		'css': 'build/css',
+		'sass': 'src/sass'
 	},
 	'spritesmith': {
 		'imgName': 'sprite.png',
@@ -41,14 +11,5 @@ module.exports = {
 		'imgOpts': {
 			'quality': 60
 		}
-	},
-	'htmlmin': {
-		'removeComments': true,
-		'collapseWhitespace': true,
-		'collapseBooleanAttributes': true,
-		'removeEmptyAttributes': true,
-		'removeStyleLinkTypeAttributes': true,
-		'minifyJS': true,
-        'minifyCSS': true
 	}
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,19 @@
 const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
+const $ = require('gulp-load-plugins')();
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
 const pump = require('pump');
-const amdOptimize = require("amd-optimize");
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
 const config = require('./config');
 
-gulp.task('move', () => {
-	gulp.src(config.dev.bower.requirejs)
-		.pipe(plugins.uglify())
-		.pipe(gulp.dest(config.dev.dest.js));
-
-	gulp.src(config.dev.bower.zepto)
-		.pipe(gulp.dest(config.dev.dest.js));
-
-	gulp.src(config.dev.bower.fastclick)
-		.pipe(gulp.dest(config.dev.dest.js));
-
-	gulp.src(config.dev.bower.normalize)
-		.pipe(gulp.dest(config.dev.dest.css));
-});
-
 gulp.task('sass', () => {
 	pump([
-		gulp.src('sass/*.scss'),
-		plugins.plumber(),
-		plugins.compass(config.compass),
-		gulp.dest('css'),
+		gulp.src('src/sass/*.scss'),
+		$.plumber(),
+		$.compass(config.compass),
+		gulp.dest('build/css'),
 		browserSync.stream()
 	]);
 });
@@ -38,52 +24,41 @@ gulp.task('refresh', () => {
 		 * you should change your own ip address,
 		 * like ip + port
 		 */
-		'proxy': "http://10.146.67.187:8038/",
+		'proxy': 'http://127.0.0.1:3838/',
 		'notify': false,
 		'port': 5386
 	});
-	gulp.watch("sass/*.scss", ['sass']);
-	gulp.watch('test.html').on('change', reload);
-	gulp.watch('js/*.js').on('change', reload);
+	gulp.watch('src/sass/*.scss', ['sass']);
+	gulp.watch('src/js/*.js', ['cmd']);
+	gulp.watch('index.html').on('change', reload);
 });
 
-gulp.task('amd', () => {
+gulp.task('cmd', () => {
 	pump([
-		gulp.src('js/*.js'),
-		plugins.plumber({
-			errorHandler: err => {
-				console.log(err.message);
-			}
-		}),
-		amdOptimize('main', config.requirejs),
-		plugins.concat('app.js'),
-		plugins.uglify(),
-		plugins.rename({
-			'suffix': '.min'
-		}),
-		gulp.dest('dist/js')
-	]);
-});
-
-gulp.task('htmlmin', () => {
-	pump([
-		gulp.src('test.html'),
-		plugins.htmlmin(config.htmlmin),
-		plugins.rename({
-			basename: 'index'
-		}),
-		gulp.dest('./'),
-		plugins.replace(/(js\/main)/, config.build.js.dist),
-		gulp.dest('./')
+		browserify({
+			entries: 'src/js/app.js',
+			debug: true
+		}).bundle(),
+		source('bundle.js'),
+		gulp.dest('build/js'),
+		browserSync.stream()
 	]);
 });
 
 /* ------------------------------------------------ */
 
+gulp.task('uglify', () => {
+	pump([
+		gulp.src('build/js/bundle.js'),
+		$.uglify(),
+		gulp.dest('build/js')
+	]);
+});
+
 // sprite generate
 gulp.task('generateSprite', () => {
 	let spriteData = gulp.src('sprite/src/*')
-		.pipe(plugins.spritesmith(config.spritesmith));
+		.pipe($.spritesmith(config.spritesmith));
 	return spriteData
 		.pipe(gulp.dest('sprite'));
 });
@@ -97,7 +72,7 @@ gulp.task('sprite', ['generateSprite'], () => {
 });
 
 // dev
-gulp.task('dev', ['sass', 'refresh']);
+gulp.task('dev', ['sass', 'cmd', 'refresh']);
 
 // build
-gulp.task('build', ['amd', 'htmlmin']);
+gulp.task('build', ['uglify']);
